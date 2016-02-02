e945df3bcc05c043028a7215ad7359fd70b74aea
砸你家玻璃 <tim_duncan@sogou.com>
2016-02-03 01:04:05 +0800

    common services

--- src/main/java/com/f2/controller/BaseController.java
+++ src/main/java/com/f2/controller/BaseController.java
@@ -3,28 +3,34 @@ package com.f2.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.f2.exceptions.AuthenticationException;
 import com.f2.exceptions.BadParameterException;
 import com.f2.exceptions.BusinessException;
 
- 
 public abstract class BaseController {
 	public abstract void authenticate(HttpServletRequest requset, HttpServletResponse response) throws AuthenticationException;
 
 	public abstract void dataValidate(HttpServletRequest requset, HttpServletResponse response) throws BadParameterException;
 
 	public abstract void postProcess(HttpServletRequest requset, HttpServletResponse response);
+	//@RequestMapping(value = "updatecorp", method = RequestMethod.GET, consumes = "application/json", produces = "application/json;charset=UTF-8")
 
-	public String execute(HttpServletRequest requset, HttpServletResponse response) throws AuthenticationException, BadParameterException, BusinessException {
+	@RequestMapping(value = "query", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String execute(HttpServletRequest requset, HttpServletResponse response)
+			throws AuthenticationException, BadParameterException, BusinessException {
+		System.out.println("------------------execute----------------------------");
 		authenticate(requset, response);
 		dataValidate(requset, response);
-		String result= doBusiness(requset,response);
-		postProcess(requset,response);
+		String result = doBusiness(requset, response);
+		postProcess(requset, response);
 		return result;
-        
+
 	}
 
-	public abstract String doBusiness(HttpServletRequest requset, HttpServletResponse reponse)throws BusinessException;
+	public abstract String doBusiness(HttpServletRequest requset, HttpServletResponse reponse) throws BusinessException;
 }
--- src/main/java/com/f2/controller/CommonController.java
+++ src/main/java/com/f2/controller/CommonController.java
@@ -1,12 +1,38 @@
 package com.f2.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@RestController
-public class CommonController {
-	@RequestMapping("/")
-	String home() {
-		return "Hello World f2";
+import org.springframework.stereotype.Controller;
+
+import com.f2.exceptions.AuthenticationException;
+import com.f2.exceptions.BadParameterException;
+import com.f2.exceptions.BusinessException;
+
+@Controller
+public class CommonController extends BaseController{
+	 
+	@Override
+	public void authenticate(HttpServletRequest requset, HttpServletResponse response) throws AuthenticationException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dataValidate(HttpServletRequest requset, HttpServletResponse response) throws BadParameterException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postProcess(HttpServletRequest requset, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String doBusiness(HttpServletRequest requset, HttpServletResponse reponse) throws BusinessException {
+		 
+		return "hello common service";
 	}
 }
